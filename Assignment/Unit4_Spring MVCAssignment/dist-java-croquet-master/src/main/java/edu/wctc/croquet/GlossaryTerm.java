229e81a99e2ae48d3599abd8eab3bc37f1caf54b
Eugene <eugenebobalo@gmail.com>
2020-09-05 20:53:48 -0500

    Finished MVC Assignment - Unit 4

--- Assignment/Unit4_Spring MVCAssignment/dist-java-croquet-master/src/main/java/edu/wctc/croquet/GlossaryTerm.java
+++ Assignment/Unit4_Spring MVCAssignment/dist-java-croquet-master/src/main/java/edu/wctc/croquet/GlossaryTerm.java
@@ -1,7 +1,15 @@
 package edu.wctc.croquet;
 
+import lombok.Data;
+
 /**
  * Complete this class to hold data from croquetGlossary.json
  */
+
+@Data
 public class GlossaryTerm {
+    private String term;
+    private String definition;
+    private String regulation;
 }
+
