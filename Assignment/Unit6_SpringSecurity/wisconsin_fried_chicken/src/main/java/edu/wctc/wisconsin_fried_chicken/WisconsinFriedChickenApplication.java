a5391c0d6428488c99b19d45b4abbdd4e9345405
Eugene Bobalo <ebobalo@ArgonInd.biz>
2020-09-23 15:22:52 -0500

    Started a SpringSecurity

---
+++ Assignment/Unit6_SpringSecurity/wisconsin_fried_chicken/src/main/java/edu/wctc/wisconsin_fried_chicken/WisconsinFriedChickenApplication.java
@@ -0,0 +1,13 @@
+package edu.wctc.wisconsin_fried_chicken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisconsinFriedChickenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WisconsinFriedChickenApplication.class, args);
+    }
+
+}
---
+++ Assignment/Unit6_SpringSecurity/wisconsin_fried_chicken/src/test/java/edu/wctc/wisconsin_fried_chicken/WisconsinFriedChickenApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.wctc.wisconsin_fried_chicken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WisconsinFriedChickenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
