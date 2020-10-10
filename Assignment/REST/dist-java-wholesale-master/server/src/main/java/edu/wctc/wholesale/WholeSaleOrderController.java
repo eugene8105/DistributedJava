337429046869e7499ef7d67966c75d457803600c
Eugene <eugenebobalo@gmail.com>
2020-10-10 17:17:32 -0500

    adde WholeSaleOrderController

---
+++ Assignment/REST/dist-java-wholesale-master/server/src/main/java/edu/wctc/wholesale/WholeSaleOrderController.java
@@ -0,0 +1,21 @@
+package edu.wctc.wholesale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import service.WholeSaleOrderService;
+
+@RestController
+public class WholeSaleOrderController {
+
+    @Autowired
+    private WholeSaleOrderService wholeSaleOrderService;
+
+    @RequestMapping("/")
+    public String showAllOrders(Model model) {
+
+        return "index";
+    }
+
+}
